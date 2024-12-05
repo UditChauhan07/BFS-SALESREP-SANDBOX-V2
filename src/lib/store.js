cdb4b7aa1f0e99ae5791bef15f4bbf9faadf227a
GauravDx01 <gaurav01.dx@gmail.com>
2024-12-05 12:52:30 +0530

    12.05.24.02

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,6 @@
-export const originAPi = process.env.REACT_APP_OA_URL || "https://staging.beautyfashionsales.com"
+export const originAPi = process.env.REACT_APP_OA_URL || "https://sandbox.beautyfashionsales.com"
 // export const originAPi = "https://dev.beautyfashionsales.com"
-// export const originAPi = "http://localhost:3001"
+// export const originAPi = "http://localhost:2611"
 
 let url = `${originAPi}/retailer/`;
 let url2 = `${originAPi}/retailerv2/`;
